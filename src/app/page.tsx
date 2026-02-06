1f535bd651580dd80834a15d5d7fcc6b107892b1
Jurate-M <dev.jurate@gmail.com>
2026-02-06 17:11:56 +0200

    Search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import styles from "./page.module.css";
 
+import Search from "@/components/search";
+
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <Search />
+      </main>
     </div>
   );
 }
