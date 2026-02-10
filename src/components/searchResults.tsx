e7673ea0df360d6eb685bec71e0fa8d33e2564e2
Jurate-M <dev.jurate@gmail.com>
2026-02-10 15:55:42 +0200

    Passing place id (from search results list) to parent component

--- src/components/searchResults.tsx
+++ src/components/searchResults.tsx
@@ -3,12 +3,18 @@ interface Result {
   name: string;
 }
 
-const SearchResults = ({ results }: { results: [Result] }) => {
+const SearchResults = ({
+  results,
+  onClick,
+}: {
+  results: [Result];
+  onClick: (arg: string) => void;
+}) => {
   return (
     <ul>
       {results.map((item) => (
         <li key={item.place_id}>
-          <button>{item.name}</button>
+          <button onClick={() => onClick(item.place_id)}>{item.name}</button>
         </li>
       ))}
     </ul>
