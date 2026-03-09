ba11fb5d6c826249b561ee03ac8dc5e6da481469
Jurate-M <dev.jurate@gmail.com>
2026-03-09 19:49:03 +0200

    Hook responsibe for getting client geolocation permissions and coordinates

---
+++ src/hooks/clientCoodrs.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { PermissionDeniedType } from "@/utils/interfaces";
+import { saveClientCoordsCookie, deleteClientCoordsCookie } from "@/actions";
+
+export function useCoords() {
+  const [denied, setDenied] = useState<PermissionDeniedType>();
+  const [reqLocation, setReqLocation] = useState("");
+
+  const success = (position: GeolocationPosition) => {
+    const { latitude, longitude } = position.coords;
+    saveClientCoordsCookie(latitude, longitude);
+    console.log("success");
+  };
+
+  const error = (error: GeolocationPositionError) => {
+    setDenied({
+      status: error.code,
+      message: error.message,
+    });
+    deleteClientCoordsCookie();
+  };
+
+  const handlePersmissions = (state: PermissionState) => {
+    setReqLocation(state);
+
+    if (state === "granted") {
+      setDenied({
+        status: false,
+        message: "",
+      });
+    }
+
+    if (state === "denied") {
+      setDenied({
+        status: true,
+        message: "Denied location",
+      });
+
+      deleteClientCoordsCookie();
+    }
+  };
+
+  useEffect(() => {
+    if (!navigator.permissions) return;
+
+    navigator.permissions
+      .query({ name: "geolocation" })
+      .then((permissionStatus) => {
+        handlePersmissions(permissionStatus.state);
+
+        // Safari: The onchange event handler is supported, but the event never fires.
+        permissionStatus.onchange = () => {
+          handlePersmissions(permissionStatus.state);
+        };
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      setDenied({
+        status: true,
+        message: "Navigator not available",
+      });
+    } else {
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  }, [reqLocation]);
+
+  return [denied];
+}
