38d880f8ffd06f49e468d4d86a2508c5665b379d
Jurate-M <dev.jurate@gmail.com>
2026-03-17 15:51:37 +0200

    OnClick event for retaining persistent url accross navigation

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -2,14 +2,14 @@ import { routes } from "@/routes";
 import NavLink from "./NavLink";
 import { Suspense } from "react";
 
-export default function Nav({}) {
+export default function Nav() {
   return (
     <nav className='border-b-2'>
       <ul className='ml-auto flex justify-end -mx-1 py-2'>
-        {routes.map((route) => {
+        {routes.map(({ name, path }) => {
           return (
-            <Suspense key={route.name}>
-              <NavLink url={route.path} name={route.name} />
+            <Suspense key={name}>
+              <NavLink name={name} url={path} />
             </Suspense>
           );
         })}
--- src/components/nav/NavLink.tsx
+++ src/components/nav/NavLink.tsx
@@ -1,18 +1,24 @@
 "use client";
 
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-import { usePathname } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 export default function NavLink({ name, url }: { name: string; url: string }) {
-  const searchParams = useSearchParams();
-  const path = usePathname();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    const params = new URLSearchParams(window.location.search);
+    router.push(`${url}?${params.toString()}`);
+  };
 
   return (
     <li className='px-1'>
       <Link
-        href={`${url}${searchParams ? `?${searchParams}` : ""}`}
-        className={`block px-4 py-2 hover:text-gray-900 hover:bg-gray-100 ${path === url ? "text-gray-950" : "text-gray-700"}`}
+        href={url}
+        onClick={handleClick}
+        className={`block px-4 py-2 hover:text-gray-900 hover:bg-gray-100 ${pathname === url ? "text-gray-950" : "text-gray-700"}`}
       >
         {name}
       </Link>
