760a5bc78df36b63338512df419a9cc8e1042ac4
Jurate-M <dev.jurate@gmail.com>
2026-03-24 11:19:58 +0200

    Hourly forecast interface

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -18,3 +18,34 @@ export interface PermissionDeniedType {
   status: boolean | number;
   message: string;
 }
+
+export interface HourlyEntry {
+  date: string;
+  weather: string;
+  icon: number;
+  summary: string;
+  temperature: number;
+  feels_like: number;
+  wind_chill: number;
+  dew_point: number;
+  wind: { speed: number; gusts: number; dir: string; angle: number };
+  cloud_cover: number;
+  pressure: number;
+  precipitation: { total: number; type: string };
+  probability: { precipitation: number; storm: number; freeze: number };
+  ozone: number;
+  uv_index: number | null;
+  humidity: number;
+  visibility: number;
+}
+
+export interface HourlyData {
+  lat: string;
+  lon: string;
+  elevation: number;
+  timezone: string;
+  units: string;
+  hourly: {
+    data: HourlyEntry[];
+  };
+}
