592fd7ef2b9a246966e9bdf7c908993a4a8f5ec3
Jurate-M <dev.jurate@gmail.com>
2026-03-24 19:16:27 +0200

    Dummy data & interfaces

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -49,3 +49,42 @@ export interface HourlyData {
     data: HourlyEntry[];
   };
 }
+
+export interface DailyEntry {
+  day: string;
+  weather: string;
+  icon: number;
+  summary: string;
+  predictability: number;
+  temperature: number;
+  temperature_min: number;
+  temperature_max: number;
+  feels_like: number;
+  feels_like_min: number;
+  feels_like_max: number;
+  wind_chill: number;
+  wind_chill_min: number;
+  wind_chill_max: number;
+  dew_point: number;
+  dew_point_min: number;
+  dew_point_max: number;
+  wind: { speed: number; gusts: number; dir: string; angle: number };
+  cloud_cover: number;
+  pressure: number;
+  precipitation: { total: number; type: string };
+  probability: { precipitation: number; storm: number; freeze: number };
+  ozone: number;
+  humidity: number;
+  visibility: number;
+}
+
+export interface DailyData {
+  lat: string;
+  lon: string;
+  elevation: number;
+  timezone: string;
+  units: string;
+  hourly: {
+    data: DailyEntry[];
+  };
+}
