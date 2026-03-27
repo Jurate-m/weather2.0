b451e6d997be76ed84be5baf5074782316aa6527
Jurate-M <dev.jurate@gmail.com>
2026-03-27 12:04:42 +0200

    Arrow down icon

--- src/components/forecast/ui/ListItemCta.tsx
+++ src/components/forecast/ui/ListItemCta.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from "react";
+import Image from "next/image";
+import Arrow from "@/assets/arrow_down.svg";
 
 export default function ListItemCta({
   buttonChildren,
@@ -17,8 +19,19 @@ export default function ListItemCta({
 
   return (
     <li className='bg-white rounded-xl'>
-      <button type='button' onClick={handleClick} className='block w-full'>
+      <button
+        type='button'
+        onClick={handleClick}
+        className='block w-full relative'
+      >
         {buttonChildren}
+        <span className='block absolute top-[50%] right-4 -translate-y-[50%]'>
+          <Image
+            src={Arrow}
+            alt={`${active ? "collapse" : "expand"}`}
+            className={`${active ? "rotate-180" : ""}`}
+          />
+        </span>
       </button>
       <div className={`p-4 ${active ? "block" : "hidden"}`}>{content}</div>
     </li>
