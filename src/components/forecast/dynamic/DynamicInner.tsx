d309d975fd2331578f37578c9d50064ee5975447
Jurate-M <dev.jurate@gmail.com>
2026-04-03 22:27:04 +0300

    Responsive design amends

--- src/components/forecast/dynamic/DynamicInner.tsx
+++ src/components/forecast/dynamic/DynamicInner.tsx
@@ -1,11 +1,11 @@
 import { HourlyEntry, DailyEntry } from "@/utils/interfaces";
 
-import Icon from "../ui/Icon";
+import Icon from "../../ui/Icon";
 
 import { formatData, formatDate } from "@/utils/functions";
 
-import List from "../ui/List";
-import ListItemCta from "../ui/ListItemCta";
+import List from "../../ui/List";
+import ListItemCta from "../../ui/ListItemCta";
 
 export default function Dropdown({
   data,
@@ -26,26 +26,32 @@ export default function Dropdown({
         const { date, icon, temperature, summary, ...rest } = entry;
 
         return (
-          <ListItemCta
-            key={`${page}-${index}`}
-            buttonChildren={
-              <div className='grid grid-cols-[1fr_max-content_max-content_max-content] gap-4 items-center '>
-                <p className='text-left font-medium'>{formatDate(date)}</p>
-                <Icon id={icon} className='w-20 h-10' />
-                <p className='font-medium'>{temperature}</p>
-              </div>
-            }
-            content={
-              <>
-                <p className='px-6 font-medium'>{summary}</p>
+          <li key={`${page}-${index}`}>
+            <ListItemCta
+              btnClass='py-2 pl-6 xs:py-4'
+              buttonChildren={
+                <div className='grid grid-cols-[max-content_max-content] justify-between xs:grid-cols-[1fr_max-content_max-content_max-content] gap-4 items-center '>
+                  <p className='text-left font-medium'>{formatDate(date)}</p>
+                  <Icon id={icon} className='w-15 h-10 hidden xs:block' />
+                  <p className='font-medium'>{temperature}</p>
+                </div>
+              }
+              content={
+                <>
+                  <div className='flex items-center py-2 px-6 xs:px-8 justify-between'>
+                    <p className='font-medium pr-4 xs:pr-0'>{summary}</p>
+                    <Icon id={icon} className='w-15 h-10 block xs:hidden' />
+                  </div>
 
-                <List
-                  data={rest}
-                  className='grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-x-6 p-4'
-                />
-              </>
-            }
-          />
+                  <List
+                    data={rest}
+                    className='
+                    xs:grid grid-cols-2 gap-x-6 p-4'
+                  />
+                </>
+              }
+            />
+          </li>
         );
       })}
     </ul>
