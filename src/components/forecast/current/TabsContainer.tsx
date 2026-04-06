0de0173b4af3fd6c6abf853a058c57b217d21f06
Jurate-M <dev.jurate@gmail.com>
2026-04-06 21:06:01 +0300

    Aria attributes

--- src/components/forecast/current/TabsContainer.tsx
+++ src/components/forecast/current/TabsContainer.tsx
@@ -14,21 +14,27 @@ export default function TabsContainer({
   index: number;
 }) {
   return (
-    <ul className='grid grid-cols-3 xs:grid-cols-[repeat(auto-fit,minmax(100px,1fr))] py-4 divide-x-4 divide-gray-950/2'>
+    <ul
+      role='tablist'
+      aria-label='Hourly forecast'
+      className='grid grid-cols-3 xs:grid-cols-[repeat(auto-fit,minmax(100px,1fr))] py-4 divide-x-4 divide-gray-950/2'
+    >
       {data.map((item, id) => {
         const formattedData = formatData(units, item);
-        const { date, temperature, icon } = formattedData;
+        const { date, temperature, icon, summary } = formattedData;
 
         return (
           <li key={String(date)} className=''>
             <button
+              role='tab'
+              aria-selected={id === index}
               onClick={() => {
                 onSelect(id);
               }}
               className={`${id === index ? "bg-gray-950/2" : ""} w-full p-4 grid grid-flow-row gap-2`}
             >
               <time dateTime={date}>{`${padHours(date)}`}</time>
-              <Icon id={icon} className='h-10' />
+              <Icon iconId={icon} title={summary} className='h-10' />
               <p className='font-bold'>{temperature}</p>
             </button>
           </li>
