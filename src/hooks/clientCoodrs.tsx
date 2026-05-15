685ec3ce8332ff9cc36d821ebbb3aba1648753a7
Jurate-M <dev.jurate@gmail.com>
2026-05-15 23:08:20 +0300

    Added TIMEOUT err message

--- src/hooks/clientCoodrs.tsx
+++ src/hooks/clientCoodrs.tsx
@@ -14,7 +14,7 @@ export function useCoords(cookiesSet: boolean) {
 
   const options = {
     // enableHighAccuracy: true,
-    // timeout: 10000, // 'The default value is Infinity, meaning that getCurrentPosition() won't return until the position is available.' - not good.
+    timeout: 10000, // 'The default value is Infinity, meaning that getCurrentPosition() won't return until the position is available.'
     maximumAge: 0,
   };
 
@@ -32,6 +32,7 @@ export function useCoords(cookiesSet: boolean) {
       deletedRef.current = true;
       await deleteClientCoordsCookie();
     }
+
     if (err) {
       setCode(`${err.code}`);
       setDenied(true);
