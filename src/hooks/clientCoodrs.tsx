0404ac6c842362f791a5ad884cd82e51941f7bb6
Jurate-M <dev.jurate@gmail.com>
2026-05-15 22:11:06 +0300

    Testing mobile

--- src/hooks/clientCoodrs.tsx
+++ src/hooks/clientCoodrs.tsx
@@ -13,8 +13,8 @@ export function useCoords(cookiesSet: boolean) {
   const deletedRef = useRef(false);
 
   const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
+    // enableHighAccuracy: true,
+    // timeout: 10000, // 'The default value is Infinity, meaning that getCurrentPosition() won't return until the position is available.' - not good.
     maximumAge: 0,
   };
 
