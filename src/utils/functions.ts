dee223d7239c7d796bb169b9b7ba50fda85f711b
Jurate-M <dev.jurate@gmail.com>
2026-06-15 12:48:23 +0300

    Helper functions and objects

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -5,36 +5,42 @@ const UNITS = new Map();
 UNITS.set("metric", {
   temperature: "\u00B0C",
   precipitation: "mm/h",
-  wind_speed: "m/s",
+  speed: "m/s",
   pressure: "hPa",
+  visibility: "km",
   humidity: "\u0025",
 });
 
 UNITS.set("us", {
   temperature: "\u00B0F",
   precipitation: "in/h",
-  wind_speed: "mph",
+  speed: "mph",
   pressure: "Hg",
+  visibility: "mi",
   humidity: "\u0025",
 });
 
 UNITS.set("uk", {
   temperature: "\u00B0C",
   precipitation: "mm/h",
-  wind_speed: "mph",
+  speed: "mph",
   pressure: "hPa",
+  visibility: "mi",
   humidity: "\u0025",
 });
 
 UNITS.set("ca", {
   temperature: "\u00B0C",
   precipitation: "mm/h",
-  wind_speed: "km/h",
+  speed: "km/h",
   pressure: "kPa",
+  visibility: "km",
   humidity: "\u0025",
 });
 
-const roundWithUnits = (value: number, units: string, unit: string) =>
+export const getUnits = (units: string, unit: string) => UNITS.get(units)[unit];
+
+export const roundWithUnits = (value: number, units: string, unit: string) =>
   `${Math.round(value)}${UNITS.get(units)[unit]}`;
 
 export function formatData(units = "metric", data: HourlyEntry | DailyEntry) {
@@ -52,6 +58,12 @@ export function formatData(units = "metric", data: HourlyEntry | DailyEntry) {
   const wind = `${roundWithUnits(data.wind.speed, units, "wind_speed")} ${data.wind.dir}`;
   const pressure = roundWithUnits(data.pressure, units, "pressure");
   const humidity = roundWithUnits(data.humidity, units, "humidity");
+  const precipitation = roundWithUnits(
+    data.precipitation.total,
+    units,
+    "humidity",
+  );
+  const visibility = roundWithUnits(data.visibility, units, "visibility");
 
   const probability = Object.entries(data.probability)
     .filter(([key, value]) => value)
@@ -65,11 +77,13 @@ export function formatData(units = "metric", data: HourlyEntry | DailyEntry) {
     date,
     icon,
     summary,
-    temperature,
+    Temperature: temperature,
     "Feels Like": feels_like,
     Wind: wind,
-    Pressure: pressure,
     Humidity: humidity,
+    Precipitation: precipitation,
+    Visibility: visibility,
+    Pressure: pressure,
     ...Object.assign({}, ...probability),
     ...("uv_index" in data
       ? { "UV Index": data.uv_index ? data.uv_index : 0 }
@@ -86,6 +100,24 @@ export const weekDays = [
   ["Fri", "Friday"],
   ["Sat", "Saturday"],
 ];
+
+export const month = [
+  ["Jan", "January"],
+  ["Feb", "February"],
+  ["Mar", "March"],
+  ["May", "May"],
+  ["Jun", "June"],
+  ["Jul", "July"],
+  ["Aug", "August"],
+  ["Sep", "September"],
+  ["Oct", "October"],
+  ["Nov", "November"],
+  ["Dec", "December"],
+];
+
+export function getMonth(date: string) {
+  return month[new Date(date).getMonth()];
+}
 
 export function getDay(date: string) {
   return new Date(date).getDay();
@@ -126,3 +158,51 @@ export function secondsUntilNextHour() {
 
   return (60 - now.getMinutes()) * 60 - now.getSeconds();
 }
+
+export function time(date: Date) {
+  return `${String(new Date(date).getHours()).padStart(2, "0")}:${String(new Date(date).getMinutes()).padStart(2, "0")}`;
+}
+
+export function astroPhase(phase: string) {
+  const joined = phase.split("_").join(" ");
+  const firstLetter = joined.charAt(0).toUpperCase();
+  const remainder = joined.slice(1);
+
+  return firstLetter + remainder;
+}
+
+export const uv_recommendations = [
+  {
+    category: "Very Low",
+    range: [0],
+    recommendation: "No protection needed.",
+  },
+  {
+    category: "Low",
+    range: [1, 2],
+    recommendation: "You can safely stay outside using minimal sun protection.",
+  },
+  {
+    category: "Moderate",
+    range: [3, 4, 5],
+    recommendation: "Seek shade during late morning through mid-afternoon.",
+  },
+  {
+    category: "High",
+    range: [6, 7],
+    recommendation:
+      "Limit time outdoors during peak sun hours. Generously apply SPF 30+ sunscreen, wear protective clothing, and seek shade.",
+  },
+  {
+    category: "Very High",
+    range: [8, 9, 10],
+    recommendation:
+      "Extra protection is essential. Minimize outdoor activities midday. Use SPF 50+ sunscreen, UV-protective clothing, a hat, and sunglasses.",
+  },
+  {
+    category: "Extreme",
+    range: [11, 12, 13],
+    recommendation:
+      "Avoid sun exposure if possible during peak hours. If outdoors, take maximum precautions: use SPF 50+ sunscreen, wear full-coverage clothing, and seek shade constantly.",
+  },
+];
