dbdba900bd97fbe0ea0d2d96903b14559d775965
Jurate-M <dev.jurate@gmail.com>
2026-06-25 13:10:21 +0300

    Entry date key variable to account for different date key names in data

--- src/components/params/forecastListing.tsx
+++ src/components/params/forecastListing.tsx
@@ -1,88 +1,15 @@
-import Image from "next/image";
-import chevron from "@/assets/chevron.svg";
-
 import { getForecast } from "@/_lib/forecast";
-import {
-  getDay,
-  getMonth,
-  weekDays,
-  getDayOfWeek,
-  time,
-  getUnits,
-} from "@/utils/functions";
 import Pagination from "../ui/Pagination";
 import Accordion from "../ui/accordion";
-import Icon from "../ui/Icon";
+import AccordionContent from "./accordionContent";
+import AccordionLabel from "./accordionLabel";
 
-const Label = ({ data, units }: { data: any; units: string }) => {
-  const { date, icon, summary, temperature } = data;
-
-  return (
-    <div className='flex gap-4 py-4 px-4'>
-      <div className='flex flex-1 justify-between items-center'>
-        <div className='text-left sm:flex gap-2 sm:gap-4 items-center'>
-          <div className='flex gap-2 sm:gap-4'>
-            <Icon iconId={icon} className='h-10 sm:h-12 sm:hidden' />
-            <time dateTime={date} className='text-sm sm:text-base sm:font-bold'>
-              {weekDays[getDayOfWeek(date)][0]} {time(date)}
-              <span className='block text-sm sm:font-medium'>
-                {getMonth(date)[0]} {getDay(date)}
-              </span>
-            </time>
-          </div>
-          <Icon iconId={icon} className='h-10 hidden sm:block' />
-          <span className='font-semibold'>{summary}</span>
-        </div>
-        <span className='font-semibold'>
-          {Math.round(temperature)}{" "}
-          <span className='text-sm'>{getUnits(units, "temperature")}</span>
-        </span>
-      </div>
-      <Image src={chevron} alt='chevron' aria-hidden='true' />
-    </div>
-  );
-};
-
-const Content = ({ data, units }: { data: any; units: string }) => {
-  const { feels_like, wind, precipitation, humidity, uv_index } = data;
-
-  const details = [
-    {
-      name: "Feels like",
-      val: Math.round(feels_like),
-      units: getUnits(units, "temperature"),
-    },
-    {
-      name: "Wind",
-      val: Math.round(wind.speed),
-      units: `${wind.dir} ${getUnits(units, "speed")}`,
-    },
-    {
-      name: "Precipitation",
-      val: Math.round(precipitation.total),
-      units: getUnits(units, "precipitation"),
-    },
-    { name: "UV Index", val: Math.round(uv_index), units: "" },
-    { name: "Humidity", val: humidity, units: getUnits(units, "humidity") },
-  ];
-
-  return (
-    <ul className='grid grid-cols-2 sm:grid-cols-3 md:flex justify-between bg-secondary py-4 px-4 gap-2 border-t border-border'>
-      {details.map((detail) => {
-        return (
-          <li key={detail.name}>
-            <h3 className='text-sm text-font-1 font-bold'>
-              {detail.name.toUpperCase()}
-            </h3>
-            <p className='font-bold'>
-              {detail.val}{" "}
-              <span className='font-medium text-sm'>{detail.units}</span>
-            </p>
-          </li>
-        );
-      })}
-    </ul>
-  );
+type ForecastListingType = {
+  name: string;
+  country: string;
+  location_id: string;
+  param: "daily" | "hourly";
+  page: number;
 };
 
 export default async function ForecastListing({
@@ -91,23 +18,26 @@ export default async function ForecastListing({
   location_id,
   param,
   page,
-}: {
-  name: string;
-  country: string;
-  location_id: string;
-  param: string;
-  page: number;
-}) {
-  const { data, units, currentPage, totalPages, pagination, length } =
-    await getForecast(param, location_id, page);
+}: ForecastListingType) {
+  const {
+    data,
+    entryDateKey,
+    units,
+    currentPage,
+    totalPages,
+    pagination,
+    length,
+  } = await getForecast(param, location_id, page);
+
+  const slug = param.slice(0, 1).toUpperCase() + param.slice(1, param.length);
+  const title = `${slug} forecast`;
+  const subtitle = `Next ${length} ${param === "daily" ? "days" : "hours"} for ${name}, ${country}`;
 
   return (
     <>
       <div className='py-6'>
-        <h1 className='text-4xl font-semibold pb-2'>Hourly forecast</h1>
-        <p className='text-sm font-light'>
-          Next {length} hours for {name}, {country}
-        </p>
+        <h1 className='text-4xl font-semibold pb-2'>{title}</h1>
+        <p className='text-sm font-light'>{subtitle}</p>
       </div>
       <div className='rounded-xl border border-font-1/10 overflow-hidden'>
         <div>
@@ -115,10 +45,19 @@ export default async function ForecastListing({
             return (
               <Accordion
                 key={item.date}
-                label={<Label data={item} units={units} />}
+                label={
+                  <AccordionLabel
+                    data={item}
+                    dateKey={entryDateKey}
+                    param={param}
+                    units={units}
+                  />
+                }
                 active={i === 0}
                 contentId={item.date}
-                content={<Content data={item} units={units} />}
+                content={
+                  <AccordionContent data={item} units={units} param={param} />
+                }
                 className='border-b border-border last:border-b-0 [&>button]:hover:bg-secondary [&_img]:transition-transform [&_img]:duration-75 [&_img]:ease-in'
                 activeStyles='[&_img]:rotate-90'
               />
