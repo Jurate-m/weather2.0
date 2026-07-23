fcc0264a58bf33c7156bdf822660e3987fa20460
Jurate-M <dev.jurate@gmail.com>
2026-07-23 17:31:27 +0300

    Removed test block class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${inter.variable} ${openSans.variable} antialiased md:py-4 block`}
+        className={`${inter.variable} ${openSans.variable} antialiased md:py-4`}
       >
         <Header />
         {children}
